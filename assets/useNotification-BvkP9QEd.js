6a14d12b8ff5ddc1dd4a8005a30ef7ef6c44a0f0
what-markeart <what-markeart@gmail.com>
2026-08-19 11:11:49 +0100

    Updates

--- assets/useNotification-BvkP9QEd.js
+++
@@ -1,1 +0,0 @@
-import{a$ as i}from"./index-Ciwwa8ZS.js";function s(){const r=i();return{showSuccess:o=>r.success(o),showError:o=>r.error(o),showInfo:o=>r.info(o),showWarning:o=>r.warning(o)}}export{s as u};
